cfdf9ff9ffbd41e9111a1598848139e9bb76926b
randyshao <randy4_55@hotmail.com>
2019-12-03 19:41:31 -0500

    Added functions to display words

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
 const startClicked = document.querySelector('.start-btn');
 const mainWindow = document.querySelector('.game-card');
+const letterClicked = document.querySelector('button');
+const secretPhrase = document.querySelector('#phrase');
+const phrases = ['Hello my name is Randy', 'Ok boomer', 'Why are you running', 'What are those', 'Peanut Butter Jelly Time'];
+let wrongGuesses = 0;
 
 startClicked.addEventListener('click', (e) => {
     e.target.parentNode.style.display = 'none';
     mainWindow.setAttribute('id', 'main');
 });
+
+function getRandomPhraseAsArray(phrases) {
+    const word = phrases[Math.floor(Math.random() * phrases.length)]
+    const wordArray = word.split('');
+    return wordArray;
+    // Gets a random phrase from the array and returns an array of characters from the string
+};
+
+function addPhraseToDisplay(phrase) {
+
+    for (i = 0; i < phrase.length; i++) {
+
+        const slot = document.createElement('li')
+        const letter = document.createTextNode(phrase[i]);
+        slot.appendChild(letter);
+        secretPhrase.appendChild(slot);
+
+        if (letter === ' ') {
+            break;
+        } else {
+            slot.className = 'letter';
+            console.log(slot);
+        }
+
+    }
+    // create empty slots to represent the length of the string
+    // loops through the array of characters
+    // for each character, create a list item and append the character to the list item, and append the li to the ul
+    // if the character is a letter, add the class "letter" to the list item
+};
+
+letterClicked.addEventListener('click', (e) => {
+    function checkLetter(letter) { 
+        // loop over the letters in the word, if the value of the letter matches with a letter thats in the phrase, make it appear and return that letter.
+        // else, replace a live heart with a dead heart and return "null"
+        // disable chosen buttons
+    };
+});
+
+function checkWin(letter) {
+    // Check to see if the number of letters with class "show", matches with the number of letters with class "letters"
+    // if the number of wrong guesses is over 5, display the you lose screen card.
+};
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+
